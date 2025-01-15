4e9a4f289c8eb96376e9a9d1963d73243316704f
Josh Birch <2312292@chester.ac.uk>
2025-01-15 12:41:32 +0000

    Add exercises and reference list on arrays; write pseudocode for portfolio task 2. - Finished  Java exercises to demonstrate the use of arrays. - Written out pseudocode for portfolio task 2 "Spelling Bee" (late).

--- java/portfolio tasks/SpellingBee.java
+++ java/portfolio tasks/SpellingBee.java
@@ -1,4 +1,38 @@
 public class SpellingBee 
 {
+    /* MAIN */
+    public static void main(String[] args) 
+    {
+        // declare variables for the middle letter and other letters
         
+        // ask user to enter the middle letter
+
+        // ask user to enter the other letters
+
+        // concatenate middle letter and other letters into a single set
+
+        // start loop to accept word until user exits
+
+            // check if user exits
+
+            // validate word w method
+    }
+    
+    /* METHOD TO CHECK IF WORD IS VALID */
+        // check if word has at least 4 letters
+
+        // check if word contains middle letter
+
+        // check if word only uses allowed letters
+
+        // if all checks = true, valid word
+
+    
+    /* METHOD TO PRINT MSGS */
+
+        // print msg to console
+
+    
+    /* METHOD TO READ USER INPUT */
+        // util scanner
 }
---
+++ java/syntax/Arrays.java
@@ -0,0 +1,22 @@
+public class Arrays 
+{
+    public static void main(String[] args) 
+    {
+
+        /* - Types can't be mixed
+           - Don't declare array on variable (in C style)
+        */
+
+        int[] marks = new int[]{23, 65, 77, 12};
+        int secondMark = marks[1];
+
+        String[] names = new String[]{"Josh", "Java", "Python", "Ruby", "Haskell"};    // Array literal
+        names[3] = "Golang";
+        String lastName = names[4];
+
+        System.out.println(secondMark);
+        System.out.println(lastName);
+        System.out.println(names[3]);
+        
+    }
+}
