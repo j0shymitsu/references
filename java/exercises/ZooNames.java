161ac7909f742f4081d68ca3cf78997115e10d70
Josh Birch <2312292@chester.ac.uk>
2025-01-19 17:06:30 +0000

    /

---
+++ java/exercises/ZooNames.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ZooNames 
+{
+    public static void main(String[] args) 
+    {
+        // initialise ArrayList for animals
+        ArrayList<String> animalList = new ArrayList<String>();    
+
+        // initialise scanner
+        Scanner inputScanner = new Scanner(System.in);
+
+        // while loop to prompt for names; check for already existing
+        while(true)
+        {
+            System.out.println("Enter new animal into database (or 'X' to exit): ");
+            String newAnimal = inputScanner.nextLine();
+            String exitCharacter = "x";
+
+            if(newAnimal == exitCharacter)
+            {
+                break;
+            }
+            else if(newAnimal)
+            {
+                System.out.println("Animal is already in database, please enter a new name.");
+            }
+
+        }
+    } 
+}
