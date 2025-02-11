d13bf2a7a642315167e12f436dccce472102fd5b
Josh Birch <2312292@chester.ac.uk>
2025-02-11 14:24:36 +0000

    Catching up on Java module - Caught up to week 06 on syntax/exercises - Refactored code for package declarations

--- src/syntax/Methods.java
+++ src/syntax/Methods.java
@@ -1,16 +1,40 @@
+package syntax;
+
 import java.util.Scanner;
 
 /* Methods are normally tied to a class; Functions are independent */
 public class Methods 
 {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) 
     {
+        // Greeting user
+        greetUser();
+        // Squaring users number
+        squareUserNumber();
+
         printMenu(3);
-        Scanner scanner = new Scanner(System.in);
         System.out.println("hello all");
 
         char someLetter = "hello".charAt(3);
-    }    
+    }
+
+    static void greetUser()
+    {
+        System.out.println("Please enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
+    }
+
+    static void squareUserNumber()
+    {
+        System.out.println("Please type your favourite number: ");
+        int number = scanner.nextInt();
+        scanner.nextLine();
+        int result = number * number;
+        System.out.println("Your favourite number squared is " + result);
+    }
 
     static void printMenu(int numberOfTimes)
     {
