802692251fa11ac0f3d57fb61730b6a3f3316477
Josh Birch <2312292@chester.ac.uk>
2025-02-13 10:56:02 +0000

    Finishes #2 - "Multiple Choice" arrays task

--- src/exercises/MultipleChoice.java
+++ src/exercises/MultipleChoice.java
@@ -6,25 +6,55 @@ public class MultipleChoice
 {
     public static void main(String[] args)
     {
-        Scanner scanner = new Scanner(System.in);
+        Scanner inputScanner = new Scanner(System.in);
+
+        String question = "";
         String[] answers = new String[4];
         int correctAnswer = 0;
+        int userAnswer = 0;
 
-        System.out.println("Please enter your multiple choice question:");
-        String userQuestion = scanner.nextLine();
-        System.out.println("Please enter answer choice 1:");
-        answers[0] = scanner.nextLine();
-        System.out.println("Please enter answer choice 2:");
-        answers[1] = scanner.nextLine();
-        System.out.println("Please enter answer choice 3:");
-        answers[2] = scanner.nextLine();
-        System.out.println("Please enter answer choice 4:");
-        answers[3] = scanner.nextLine();
-        System.out.println("Please enter the number of the correct answer:");
-        correctAnswer = scanner.nextInt();
+        System.out.println("Please enter the question: ");
+        question = inputScanner.nextLine();
 
-        System.out.println("Please clear the terminal.");
+        for (int i = 0; i < answers.length; i++)
+        {
+            int currentQuestion = i + 1;
+            System.out.println("Please enter the answer for question " + currentQuestion + ":");
+            answers[i] = inputScanner.nextLine();
+        }
 
+        System.out.println("Please enter the number of the correct answer: ");
+        correctAnswer = inputScanner.nextInt();
 
+        System.out.println("\nQuestions and answers saved!");
+        System.out.println();
+
+        while(true)
+        {
+            System.out.println("\n\n\n\n\n\n\n\n\n\n");
+            System.out.println("Time to guess!\n");
+            System.out.println("Question: " + question);
+            System.out.println("Answers: ");
+
+            for (int i = 0; i < answers.length; i++)
+            {
+                int answerNumber = i + 1;
+                System.out.println(answerNumber + ". " + answers[i]);
+            }
+
+            System.out.println("\nEnter the number of your guess!");
+            userAnswer = inputScanner.nextInt();
+
+            if(userAnswer == correctAnswer)
+            {
+                System.out.println("Correct! The answer was number " + userAnswer);
+                break;
+            }
+            else
+            {
+                System.out.println("Incorrect! The answer was number " + correctAnswer);
+                break;
+            }
+        }
     }
 }
