9a538d60b701456b5695706376651d404d29acd5
Josh Birch <joshuabirch@protonmail.com>
2025-09-03 12:32:40 +0100

    Add more notes on match statement

--- python/syntax/match.py
+++ python/syntax/match.py
@@ -16,3 +16,26 @@ def get_hex(colour):
         # default case (invalid)
         case _:
             return "#FFFFFF"
+        
+###
+
+class DocFormat(Enum):
+    PDF = 1
+    TXT = 2
+    MD = 3
+    HTML = 4
+
+def convert_format(content, from_format, to_format):
+    match (from_format, to_format):
+        case (DocFormat.MD, DocFormat.HTML):
+            content = content.replace("# ", "<h1>")
+            content += "</h1>"
+            return content
+        case(DocFormat.TXT, DocFormat.PDF):
+            return "[PDF] " + content + " [PDF]" 
+        case(DocFormat.HTML, DocFormat.MD):
+            content = content.replace("<h1>", "# ")
+            content = content.replace("</h1>", "")
+            return content 
+        case _:
+            raise Exception("invalid type")
