5689bc7760c6f92d213ac3c6e910c9e2c85f07d7
Josh Birch <joshuabirch@protonmail.com>
2026-01-03 16:35:34 +0000

    Add references for using wxWidgets in CPP (GUI), incomplete

---
+++ cpp/gui/App.h
@@ -0,0 +1,18 @@
+//
+// Created by josh on 1/3/26.
+//
+
+#ifndef REFERENCES_APP_H
+#define REFERENCES_APP_H
+#pragma once
+#include <wx/wx.h>
+
+
+class App : public wxApp
+{
+    public:
+        bool OnInit();
+};
+
+
+#endif //REFERENCES_APP_H
---
+++ cpp/gui/MainFrame.cpp
@@ -0,0 +1,12 @@
+//
+// Created by josh on 1/3/26.
+//
+
+#include "MainFrame.h"
+#include <wx/wx.h>
+
+MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title)
+{
+    // Entry point in a wx window program: Class that represents application
+
+}
