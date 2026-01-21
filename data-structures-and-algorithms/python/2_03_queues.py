99b1d0e01a50bd69c7498ad67865d9d1d0639951
Josh Birch <joshuabirch@protonmail.com>
2026-01-21 06:31:10 +0000

    Finish Python notes on queues DSA

--- data-structures-and-algorithms/python/2_03_queues.py
+++ data-structures-and-algorithms/python/2_03_queues.py
@@ -8,6 +8,7 @@ class Queue:
 
 
     # Also called 'enqueue' when referring to queues
+    ## This example is not O(1)
     def push(self, item):
         self.items.insert(0, item)
 
@@ -26,3 +27,19 @@ class Queue:
 
     def size(self):
         return len(self.items)
+    
+def matchmake(queue, user):
+    username = user[0]
+    matchmaking_status = user[-1]
+
+    if matchmaking_status == 'leave':
+        queue.search_and_remove(username)
+    elif matchmaking_status == 'join':
+        queue.push(username)
+
+    if queue.size() >= 4:
+        user_1 = queue.pop()
+        user_2 = queue.pop()
+        return f'{user_1} matched {user_2}!'
+    elif queue.size() < 4:
+        return f'No match found'
