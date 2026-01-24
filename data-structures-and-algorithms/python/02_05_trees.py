92e2048b668e23771dd626aa43ad6d31f5424016
Josh Birch <joshuabirch@protonmail.com>
2026-01-24 08:11:39 +0000

    Finish BSTNode (binary search tree) class with example methods for ordering, returning values, and height checking

--- data-structures-and-algorithms/python/02_05_trees.py
+++ data-structures-and-algorithms/python/02_05_trees.py
@@ -37,3 +37,59 @@ class BSTNode:
         while current.right is not None:
             current = current.right
         return current.val
+    
+    def preorder(self, visited):
+        ''' Current node visited before its children '''
+        if self.val is not None:
+            visited.append(self.val)
+        if self.left:
+            self.left.preorder(visited)
+        if self.right:
+            self.right.preorder(visited)
+        return visited
+    
+    def postorder(self, visited):
+        ''' Current node visited after its children '''
+        if self.left:
+            self.left.postorder(visited)
+        if self.right:
+            self.right.postorder(visited)
+        visited.append(self.val)
+        return visited
+    
+    def inorder(self, visited):
+        ''' Current node visited in between its children '''
+        if self.left:
+            self.left.inorder(visited)
+        visited.append(self.val)
+        if self.right:
+            self.right.inorder(visited)
+        return visited
+    
+    def exists(self, val):
+        ''' Return true if value exists in tree '''
+        if val == self.val:
+            return True
+        if val < self.val:
+            if self.left and self.left.exists(val):
+                return True
+            else:
+                return False
+        if val > self.val:
+            if self.right and self.right.exists(val):
+                return True
+            else:
+                return False
+        return False
+    
+    def height(self):
+        ''' Returns height of tree rooted at current node '''
+        if self.val is None:
+            return 0
+        left_height = 0
+        right_height = 0
+        if self.left is not None:
+            left_height = self.left.height()
+        if self.right is not None:
+            right_height = self.right.height()
+        return max(left_height, right_height) + 1
