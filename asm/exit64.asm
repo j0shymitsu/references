5daabadd00c2f0a84f5c2e3a89908ac0e40f834e
Josh Birch <joshuabirch@protonmail.com>
2026-02-25 13:16:40 +0000

    Update .gitignore and add assembly notes

--- asm/exit64.asm
+++ asm/exit64.asm
@@ -1,8 +1,25 @@
-section .text
+; First program
 
-global _start
+; syscall_64.tbl    --64-bit system call numbers and entry vectors
+; syscall.h         --Linux syscall interfaces (non-arch-specific)
+
+segment .text
+  global _start
+
 _start:
-  mov rax, 60       ; the exit() call number
-  mov rdi, 5        ; 5  // something to look at
-  syscall           ; make the call
+  mov rax, 60       ; use syscall_64 look for 60 sys_exit
+                    ; sets the system call number for sys_exit in the rax register
+                    ; the system call number for exit is 60
+
+  mov rdi, 4        ; use syscall.h   error_code
+                    ; long sys_exit(int error_code);
+                    ; sets the error code (first parameter) to 4 in the rdi register
+
+                    ; rdi: first parameter
+                    ; rsi: second parameter
+                    ; rdx: third parameter
+                    ; rcx: fourth parameter
+                    ; r8: fifth parameter
+                    ; r9: sixth parameter
+  syscall           
 
